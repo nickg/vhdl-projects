cb1c238a411097b94e2840c3f88e1b10a5ce9494
Nick Gasson <nick@nickg.me.uk>
2022-07-09 22:12:14 +0100

    Enable more UVVM tests

--- uvvm/test.sh
+++ uvvm/test.sh
@@ -7,7 +7,7 @@ STD=2008
 
 A_OPTS=
 E_OPTS=
-R_OPTS="--stop-delta=3000"
+R_OPTS="--stop-delta=3000 --exit-severity=failure"
 
 GHDL_OPTS=
 
@@ -16,7 +16,10 @@ analyse bitvis_uart/tb/uart_vvc_demo_tb.vhd
 
 analyse bitvis_irqc/tb/irqc_demo_tb.vhd
 
-for TOP in uart_vvc_demo_tb uart_vvc_demo_tb; do
+analyse bitvis_vip_uart/tb/uvvm_demo_th.vhd
+analyse bitvis_vip_uart/tb/uvvm_demo_tb.vhd
+
+for TOP in uart_vvc_demo_tb uart_vvc_demo_tb uvvm_demo_tb; do
   elaborate
   run
 done
